2b915660dbabb22562cdc55a05ac782fec58ea91
tokitsu <toki2222son@yahoo.co.jp>
2021-06-14 18:19:33 +0900

    add crud function

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -1,10 +1,9 @@
 class Api::V1::EmployeesController < ApiController
-  before_action :set_employee, only: [:show]
+  before_action :set_employee, only: [:show, :update, :destroy]
 
-  # ActiveRecordのレコードが見つからなければ404 not foundを応答する
-  rescue_from ActiveRecord::RecordNotFound do |exception|
-    render json: { error: '404 not found' }, status: 404
-  end
+  rescue_from Exception, with: :render_status_500
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_status_404
 
   def index
     employees = Employee.select(:id, :name, :department, :gender)
@@ -15,9 +14,42 @@ class Api::V1::EmployeesController < ApiController
     render json: @employee
   end
 
-  private
+  def create
+    employee = Employee.new(employee_params)
+    if employee.save
+      render json: employee, status: :created
+    else
+      render json: { errors: employee.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 
+  def update
+    if @employee.update(employee_params)
+      head :no_content
+    else
+      render json: { errors: @employee.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @employee.destroy!
+    head :no_content
+  end
+
+  private
     def set_employee
       @employee = Employee.find(params[:id])
+    end
+
+    def employee_params
+      params.fetch(:employee, {}).permit(:name, :department, :gender, :birth, :joined_date, :payment, :note, :id, :created_at, :updated_at, :lock_version)
+    end
+
+    def render_status_404(exception)
+      render json: { errors: [exception] }, status: 404
+    end
+
+    def render_status_500(exception)
+      render json: { errors: [exception] }, status: 500
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   namespace :api, {format: 'json'} do
     namespace :v1 do
-      resources :employees, only: [:index, :show]
+      resources :employees, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
