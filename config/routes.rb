277959fa01fe63e69dc636c28929e9ad6e03766c
tokitsu <toki2222son@yahoo.co.jp>
2021-06-14 14:45:48 +0900

    frontend style and axios

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, {format: 'json'} do
+    namespace :v1 do
+      resources :employees, only: [:index, :show]
+    end
+  end
 end
