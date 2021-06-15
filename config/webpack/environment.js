7ae277071d2de045ed3a4c3625994d0d385e2a3e
tokitsu <toki2222son@yahoo.co.jp>
2021-06-15 21:54:04 +0900

    add alias

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,5 +3,14 @@ const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+
+const config = environment.toWebpackConfig()
+
+config.resolve.alias = {
+  // Vue の runtime build を有効にする
+  'vue$': 'vue/dist/vue.esm.js'
+}
+
+
 environment.loaders.prepend('vue', vue)
 module.exports = environment
